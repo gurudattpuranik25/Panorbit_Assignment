2a41b99bf6b97dc80f4e571668ae49e483cba9b3
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2023-05-12 19:04:38 +0530

    removed comments from stylesheet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import "../styles/home.css";
 import loader from "../assets/loader.gif";
 import UserAccount from "../components/UserAccount";
